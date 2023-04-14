6943f5ce206d8d2b9e022571bff0a21a8b49d620
johnryanmal <johnryanmal@gmail.com>
2023-04-14 11:20:38 -0700

    add spacing

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import Header from '@/components/Header.vue'
       }
     ]"
   />
-  <div class="container">
+  <div class="animate-size pt-2 px-2 pt-sm-3 px-sm-3 pt-md-4 px-md-4 pt-xl-5 px-xl-5">
     <RouterView />
   </div>
 </template>
